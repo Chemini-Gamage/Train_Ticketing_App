9c5b3298b02a78dd32d75669a4d79f48ba18f6fc
CheminiK <cgamage2023@gmail.com>
2024-06-15 17:49:09 +0530

    checkboxes added.

--- frontend/src/pages/TrainManagement/AdminTrainList/AdminTrainList.js
+++ frontend/src/pages/TrainManagement/AdminTrainList/AdminTrainList.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import './AdminTrainList.css'
 function AdminTrainList() {
     const [train, setTrain] = useState([])
+
+    const [selectedRows, setSelectedRows] = useState([])
     function getTrain() {
         axios.get(`http://localhost:8070/train/display`).then((resp) => {
             setTrain(resp.data)
@@ -44,16 +46,35 @@ function AdminTrainList() {
         if (hour == 0) hour = 12;
         return `${hour}:${minute} ${period}`;
     }
-
-
+    const toggleRowSelection = (id) => {
+        if (selectedRows.includes(id)) {
+            setSelectedRows(selectedRows.filter(rowId => rowId !== id));
+        } else {
+            setSelectedRows([...selectedRows, id])
+        }
+    }
+    const handleDeleteSelected = () => {
+        console.log("deleting", selectedRows)
+    }
     return (
-        <div className="display" style={{ marginTop: "20px", marginLeft: '50px', marginRight: '50px' }}>
+        <div className="displayA" style={{ marginTop: "20px", marginLeft: '50px', marginRight: '50px' }}>
             <h5>Admin list of trains</h5>
-            <a href="/add" class="button">Add train</a>
+
+
+            <nav>
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
+
+            <Link to="/train/viewMore" className="btn btn-success" style={{ marginLeft: '1000px', marginTop: "-10px" }}>Add</Link>
+            <button className="btnr" onClick={handleDeleteSelected}>Delete Selected</button>
             <hr></hr>
             <table class="table table-bordered" >
                 <thead>
                     <tr>
+                        <th scope="col">Select</th>
                         <th scope="col">Index</th>
                         <th scope="col">Train Code</th>
                         {/* assign a letter for the trains */}
@@ -61,9 +82,11 @@ function AdminTrainList() {
                         <th scope="col">Train Departure At</th>
                         <th scope="col">Train destination</th>
                         <th scope="col">Train Departure time</th>
-                        <th scope="col">Train url</th>
+                        {/* <th scope="col">Train url</th> */}
+
                         <th scope="col">Train avaialbiltiy</th>
                         <th scope="col">Ticket price in RS</th>
+
                         <th scope="col">Action</th>
                     </tr>
 
@@ -71,7 +94,7 @@ function AdminTrainList() {
                 <tbody>
                     {train.map((train, index) => (
                         <tr key={train._id} className='col-sm-3 mb-3'>
-
+                            <td><input type="checkbox" onChange={() => toggleRowSelection(train._id)}></input></td>
                             <td>{index + 1}</td>
                             <td>{"TR" + train.trainCode}</td>
                             <td>{train.trainName}</td>
@@ -79,11 +102,12 @@ function AdminTrainList() {
                             <td>{train.destination}</td>
                             <td>{formatTime(train.departureTime)}</td>
 
-                            <td>{train.trainUrl}</td>
+                            {/* <td>{train.trainUrl}</td> */}
                             <td>{train.availability}</td>
                             {/* use a checkbox or sth */}
                             <td>{train.ticketPrice}</td>
                             <td>
+                                <Link to="/train/viewMore" className="btn btn-primary">View more</Link>
                                 <Link to={`/update/${train._id}`} className="btn btn-success">Update</Link>
                                 <button className="btn btn-danger" onClick={() => { handleDelete(train._id) }}>Delete</button>
                             </td>
